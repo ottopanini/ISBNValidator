fb4a962cdfd790735d9efa8b6a9da16d6f7b7b79
Christian Preß <Christian.Press@syracos.com>
2020-05-10 00:31:28 +0200

    initial commit

---
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest4.java
@@ -0,0 +1,13 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateISBNTest4 {
+
+    @Test
+    public void test() {
+        //fail();
+    }
+}
---
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest5.java
@@ -0,0 +1,13 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateISBNTest5 {
+    @Test
+    public void test() {
+        //fail();
+    }
+
+}
