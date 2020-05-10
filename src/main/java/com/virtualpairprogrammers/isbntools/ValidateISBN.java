e1ce69b7bb9d461c2835be0a3aa31f71ec78f00f
Christian Preß <Christian.Press@syracos.com>
2020-05-10 14:19:13 +0200

    Why you must always start with a failing test! Part IV - tests just pass

--- src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,6 +2,8 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
     public boolean checkISBN(int isbn) {
-        return true;
+        if (isbn == 140449116)
+            return true;
+        return false;
     }
 }
