d3fc20ed035c14306cb063d1cfc2357781d71c6c
Christian Preß <Christian.Press@syracos.com>
2020-05-10 11:58:08 +0200

    Step 1 - getting to red: red part of the red green blue cycle solved

--- src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -1,4 +1,7 @@
 package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
+    public boolean checkISBN(int isbn) {
+        return false;
+    }
 }
--- src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest4.java
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest4.java
@@ -5,9 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ValidateISBNTest4 {
-
     @Test
     public void checkAValidISBN() {
-        fail();
+        ValidateISBN validator = new ValidateISBN();
+        //the original number was with a heading Zero: "0140449116" but did'nt work with integer parameter
+        boolean result = validator.checkISBN(140449116);
+
+        assertTrue(result);
     }
 }
